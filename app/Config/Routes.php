545112dc7485c41403987f0151f66aa4ece605cb
Pramudya76 <pramudyarama76@gmail.com>
2025-05-22 19:42:39 +0700

    Pertemuan 10 & 11 CRUD

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -11,7 +11,19 @@ $routes->get('login', 'AuthController::login');
 $routes->post('login', 'AuthController::login');
 $routes->get('logout', 'AuthController::logout');
 
-$routes->get('produk', 'Home::produk', ['filter' => 'auth']);
+// $routes->get('produk', 'Home::produk', ['filter' => 'auth']);
+// $routes->post('produk', 'ProdukController::create', ['filter' => 'auth']);
+// $routes->post('produk/edit/(:any)', 'ProdukController::edit/$1', ['filter' => 'auth']);
+// $routes->get('produk/delete/(:any)', 'ProdukController::delete/$1', ['filter' => 'auth']);
+
+$routes->group('produk', ['filter' => 'auth'], function ($routes) { 
+    $routes->get('', 'ProdukController::index');
+    $routes->post('', 'ProdukController::create');
+    $routes->post('edit/(:any)', 'ProdukController::edit/$1');
+    $routes->get('delete/(:any)', 'ProdukController::delete/$1');
+});
+$routes->post('produk', 'ProdukController::create', ['filter' => 'auth']);
+
 $routes->get('keranjang', 'Home::keranjang', ['filter' => 'auth']);
 $routes->get('stokBarang', 'Home::stokBarang', ['filter' => 'auth']);
 $routes->get('pelanggan', 'Home::pelanggan', ['filter' => 'auth']);
